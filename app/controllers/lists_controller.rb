795d8289885f6512457b909b94eab65ec778e9e5
shangweilin <shangweilin7@gmail.com>
2017-05-01 00:23:27 +0800

    DRY set_list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
   def index
     @lists = List.where(complete: false)
   end
@@ -18,16 +20,12 @@ class ListsController < ApplicationController
   end
 
   def show
-    @list = List.find(params[:id])  
   end
 
   def edit
-    @list = List.find(params[:id])
   end
 
   def update
-    @list = List.find(params[:id])
-
     if @list.update(list_params)
       redirect_to list_path(@list)
     else
@@ -36,12 +34,15 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:id])
     @list.destroy
     redirect_to lists_path
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:title, :date, :complete)
