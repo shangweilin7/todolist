cefbb7fe91194f73042276de4468010221402c7a
shangweilin <shangweilin7@gmail.com>
2017-04-30 23:09:48 +0800

    add list edit and update action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,6 +21,20 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])  
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def list_params
