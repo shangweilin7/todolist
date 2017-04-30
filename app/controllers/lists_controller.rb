877cd574cca8ab454583d26eb2562e54dead749f
shangweilin <shangweilin7@gmail.com>
2017-04-30 22:42:10 +0800

    add list new and create action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,24 @@ class ListsController < ApplicationController
   def index
     @lists = List.where(complete: false)
   end
+
+  def new 
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :date, :complete)
+  end
 end
